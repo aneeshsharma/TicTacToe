0ef0d5b718f04fcd7166ea56d3508fe1ac6f8b9d
Anish Sharma <aneeshsharma15@outlook.com>
2021-01-12 13:59:06 +0530

    implemented win condition

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,16 +4,69 @@ import '../stylesheets/board.css';
 
 const initialBoard = Array(9).fill(null);
 
+const winSeq = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             boardState: initialBoard,
             turn: 'X',
+            active: true,
         };
     }
 
+    componentDidUpdate() {
+        if (!this.state.active) {
+            return;
+        }
+        if (this.state.turn === 'X') {
+            if (this.checkWinCondition('O')) {
+                this.setState({
+                    active: false,
+                });
+                alert('O wins');
+            }
+        }
+        if (this.state.turn === 'O') {
+            if (this.checkWinCondition('X')) {
+                this.setState({
+                    active: false,
+                });
+                alert('X wins');
+            }
+        }
+    }
+
+    checkWinCondition = (turn) => {
+        var board = this.state.boardState;
+        var flag = false;
+        for (var seq of winSeq) {
+            flag = true;
+            for (var i of seq) {
+                if (board[i] != turn) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+        return false;
+    };
+
     handleMove = (index) => {
+        if (!this.state.active) return;
         var turn = this.state.turn;
         var boardState = this.state.boardState;
         if (boardState[index] == null) boardState[index] = turn;
@@ -48,7 +101,7 @@ class Board extends React.Component {
     render() {
         let grid = this.renderGrid();
 
-        return grid;
+        return <div className="container">{grid}</div>;
     }
 }
 
